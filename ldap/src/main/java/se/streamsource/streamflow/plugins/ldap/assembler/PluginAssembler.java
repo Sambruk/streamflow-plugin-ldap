64d5029e6c4f376d2942f1178c4d434ff29c0451
Henrik Reinhold <henrik.reinhold@jayway.com>
2010-10-07 15:40:03 +0200

    Add initial maven project layout and files based on the template plugin

---
+++ ldap/src/main/java/se/streamsource/streamflow/plugins/ldap/assembler/PluginAssembler.java
@@ -0,0 +1,26 @@
+package se.streamsource.streamflow.plugins.ldap.assembler;
+
+import org.qi4j.api.common.Visibility;
+import org.qi4j.bootstrap.Assembler;
+import org.qi4j.bootstrap.AssemblyException;
+import org.qi4j.bootstrap.ModuleAssembly;
+import se.streamsource.streamflow.commercial.ldap.contact.LdapContactLookupPlugin;
+import se.streamsource.streamflow.commercial.ldap.contact.LdapContactLookupPluginConfiguration;
+
+/**
+ * Register the Ldap plugin in the plugin application
+ */
+public class PluginAssembler
+      implements Assembler
+{
+   public void assemble( ModuleAssembly module ) throws AssemblyException
+   {
+      module.addServices( LdapContactLookupPlugin.class ).
+            identifiedBy( "ldapcontactlookup" ).
+            visibleIn( Visibility.application ).
+            instantiateOnStartup();
+
+      module.addEntities( LdapContactLookupPluginConfiguration.class ).visibleIn( Visibility.application );
+
+   }
+}
---
+++ ldap/src/test/java/se/streamsource/streamflow/commercial/ldap/contact/LdapContactLookupPluginTest.java
@@ -0,0 +1,37 @@
+package se.streamsource.streamflow.commercial.ldap.contact;
+
+import org.junit.Test;
+import org.qi4j.api.common.Visibility;
+import org.qi4j.api.injection.scope.Service;
+import org.qi4j.api.unitofwork.UnitOfWork;
+import org.qi4j.bootstrap.AssemblyException;
+import org.qi4j.bootstrap.ModuleAssembly;
+import org.qi4j.test.AbstractQi4jTest;
+import se.streamsource.streamflow.server.plugin.contact.ContactLookup;
+
+/**
+ * Test for LdapContactLookup plugin. Using an in-memory Apache Ds.
+ */
+public class LdapContactLookupPluginTest extends AbstractQi4jTest
+{
+   public void assemble( ModuleAssembly module ) throws AssemblyException
+   {
+      module.layerAssembly().applicationAssembly().setName( getClass().getSimpleName() );
+      module.addServices( LdapContactLookupPlugin.class ).
+            identifiedBy( "ldapcontactlookup" ).
+            visibleIn( Visibility.application ).
+            instantiateOnStartup();
+
+      module.addEntities( LdapContactLookupPluginConfiguration.class ).visibleIn( Visibility.application );
+   }
+
+   @Service
+   ContactLookup contactLookup;
+
+   @Test
+   public void testLookup()
+   {
+      contactLookup.lookup( null );  
+   }
+
+}
