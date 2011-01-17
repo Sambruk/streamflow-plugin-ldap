b6c0c9760f7e4d90b078b49c8b2a7ea702d62414
Arvid Huss <arvid.huss@jayway.com>
2011-01-17 16:05:20 +0100

    Release preparations Changed project structure and added distribution module

--- plugin/src/main/java/se/streamsource/streamflow/plugins/ldap/assembler/PluginAssembler.java
+++ plugin/src/main/java/se/streamsource/streamflow/plugins/ldap/assembler/PluginAssembler.java
@@ -11,7 +11,6 @@ import org.qi4j.api.common.Visibility;
 import org.qi4j.bootstrap.Assembler;
 import org.qi4j.bootstrap.AssemblyException;
 import org.qi4j.bootstrap.ModuleAssembly;
-
 import se.streamsource.streamflow.infrastructure.configuration.FileConfiguration;
 import se.streamsource.streamflow.plugins.ldap.LoggingService;
 import se.streamsource.streamflow.plugins.ldap.authentication.LdapAuthenticatePlugin;
